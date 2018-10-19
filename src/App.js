b07e0d25e38d1f9f2f38b480a2261151495fbce8
Stefan <shodgeskluck@gmail.com>
2018-10-19 10:07:13 -0400

    Create a React app and publish it to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,28 +1,63 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './markdown.scss';
 
-class App extends Component {
+const marked = require("marked");
+
+marked.setOptions({
+  breaks: true,
+});
+
+class MarkdownViewer extends Component {
+constructor(props) {
+  super(props);
+  this.state = {
+    markdown: placeholder
+  }
+  this.handleChange = this.handleChange.bind(this);
+}
+
+handleChange(event) {
+  this.setState({
+    markdown: event.target.value
+  })
+}
+ 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="MarkdownPreviewer">
+        <header className="Header">Editor</header>
+          <div className="Editor-div">
+            <textarea id="editor" value={this.state.markdown} onChange={this.handleChange}>
+              I am a text editor!
+            </textarea>
+          </div>
+        <header className="Header">Preview</header>
+          <div className="Preview-div">
+            <div id="preview" dangerouslySetInnerHTML = {{__html: marked(this.state.markdown)}} />
+          </div>
       </div>
     );
   }
 }
 
-export default App;
+const placeholder = 
+`# This is a header.
+## This is a sub-header.
+This is some __bold__ text.
+[This is a link to an awesome site.](https://stefanhk31.github.io/)
+This is some code: \`function()\`
+This is a code block: 
+\`\`\`python
+str = "Hi, I'm Python!"
+print str
+\`\`\`
+1. Here is a list.
+2. It has multiple items.
+> This is a block quote,
+it continues on to the next line.
+I made this with React. ![React Logo w/ Text](https://goo.gl/Umyytc)
+
+` 
+
+export default MarkdownViewer;
